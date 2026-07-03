23c31b41472ce2eba71a7acfe5a3b1d8d247560e
ChiragSathwaraInfotech <chirag.sathwarainfotech@gmail.com>
2026-07-03 12:58:03 +0530

    feat: add firebase google-services configuration and integrate web assets for android build

--- android/app/src/main/assets/public/assets/web-C-QahDcm.js
+++ android/app/src/main/assets/public/assets/web-C-QahDcm.js
@@ -1,4 +1,4 @@
-import{W as mt}from"./index-yu3PceoB.js";const bt=()=>{};var ge={};/**
+import{W as mt}from"./index-CBhXOiBL.js";const bt=()=>{};var ge={};/**
  * @license
  * Copyright 2017 Google LLC
  *
